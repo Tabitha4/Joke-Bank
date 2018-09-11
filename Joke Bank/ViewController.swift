6cae9896ae86ff099610c84890fbeabed75e5fd3
Tabitha4 <tabithalevine@hotmail.co>
2018-09-10 20:24:07 -0400

    Initial Commit

---
+++ Joke Bank/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Joke Bank
+//
+//  Created by Tabitha Levine on 2018-09-10.
+//  Copyright © 2018 Tabitha Levine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
